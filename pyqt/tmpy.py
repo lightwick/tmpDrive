efd9ef5263a20c1552bea477dfab8c2bece2c74d
LW <17444139+lightwick@users.noreply.github.com>
2021-02-05 15:47:31 +0900

    made parent class 'character'
    all the character classes (i.e. monster & main character) now inherits character class

--- pyqt/tmpy.py
+++ pyqt/tmpy.py
@@ -7,31 +7,95 @@ import sys
 import random
 
 class myWidget(QtWidgets.QWidget):
-    
-    class Monster:
-        def __init__(self, mainWidget):
-            self.parent = mainWidget
-            self.col_num = 8
-            self.row_num = 4
+    class character:
+        def __init__(self, parent, row, col, SpriteSheetPath, TimeInterval=50):
+            self.parent = parent
+            self.row_num = row
+            self.col_num = col
             self.frame_x = 0
-            self.SpriteSheet = QPixmap("./img/monster.png")
+            self.frame_y = 0
+            self.SpriteSheet = QPixmap(SpriteSheetPath)
             self.xDist = self.SpriteSheet.width()/self.col_num
             self.yDist = self.SpriteSheet.height()/self.row_num
+            self.CharX=500
             
+            self.go = True
             
+            self.timer = QTimer(self.parent)
+            self.timer.setInterval(TimeInterval)
+            self.timer.timeout.connect(self.timeout)
+            self.timer.start()
+            
+        def draw(self, painter):
+            painter.drawPixmap(self.CharX,585, self.SpriteSheet, self.frame_x, self.frame_y, self.xDist, self.yDist)
+                
+        def timeout(self):
+            self.CharSetNextFrame()
+            self.parent.update()
+
+        def BackNForth(self):
+            self.frame_x += self.xDist
+
+            if self.go:
+                self.frame_y = self.yDist*7
+                self.CharX +=15
+            else:
+                self.frame_y = self.yDist*5
+                self.CharX -=15
+            if self.CharX > self.parent.width()-self.xDist:
+                self.go=False
+            if self.CharX<=0:
+                self.go=True
+                
+            if self.frame_x >= self.xDist*self.col_num:
+                self.frame_x=0
+        
+        def CharSetNextFrame(self):
+            self.frame_x += self.xDist
+            if self.frame_x >= self.xDist*self.col_num:
+                self.frame_x=0
+            
+    class MainCharacter(character):
+        def __init__(self, **kwargv):
+            super().__init__(**kwargv)
+            #self.timer.stop()
+            self.frame_y = 0
+            self.CharX = 0
+            self.count = 0
+        
+        def keyPressEvent(self, event):
+            if event.key()==Qt.Key_Right:
+                self.frame_y = self.yDist*7
+                self.CharX += 10
+            elif event.key()==Qt.Key_Left:
+                self.frame_y = self.yDist*5
+                self.CharX -= 10
+            elif event.key()==Qt.Key_Up:
+                self.frame_y = self.yDist * 6
+            elif event.key()==Qt.Key_Down:
+                self.frame_y = self.yDist * 4
+            
+            self.CharSetNextFrame()
+
+        def keyReleaseEvent(self, event):
+            if not event.isAutoRepeat():
+                print("released")
+                self.frame_x = 0
+                if self.frame_y >= self.yDist*4:
+                    self.frame_y -= self.yDist*4
+            
+        def timeout(self):
+            self.BackNForth()
+            self.parent.update()
+            
+    class Monster(character):
+        def __init__(self, **kwargv):
+            super().__init__(**kwargv)              
             #DEBUG
             self.frame_y = 0
             self.count=0
             self.CharX = 500
-            
-            self.timer = QTimer(self.parent)
-            self.timer.setInterval(75)
-            self.timer.timeout.connect(self.timeout)
-            self.timer.start()
-            
-        def _draw(self,painter):
-            painter.drawPixmap(self.CharX,360, self.SpriteSheet, self.frame_x, self.frame_y, self.xDist, self.yDist)
-            
+        
         def CharSetNextFrame(self):
             self.frame_x += self.xDist
             #self.CharX +=5
@@ -46,13 +110,7 @@ class myWidget(QtWidgets.QWidget):
                 self.count=0
                 self.frame_y=self.yDist*2
                 self.frame_x=0
-            
-        def timeout(self):
-            print("monster")
-            self.CharSetNextFrame()
-            self.parent.update()
-            
-        
+
             
     def __init__(self):
         super(myWidget,self).__init__()
@@ -62,77 +120,33 @@ class myWidget(QtWidgets.QWidget):
         
     def initUI(self):
         self.setWindowTitle("walking animation test")
-        self.resize(900, 600)
+        self.resize(1500, 900)
     
     def initCharacter(self):
         # machine = QtCore.QStateMachine()
-        self.col_num=10
-        self.row_num=8
         self.frame_x=0
-        self.frame_y=0
-        self.SpriteSheet = QPixmap("./img/link.png")
-        self.xDist = self.SpriteSheet.width()/self.col_num
-        self.yDist = self.SpriteSheet.height()/self.row_num
-        
         
         #DEBUG
-        self.frame_y = self.yDist*7
         self.count = 0
-        self.monster = self.Monster(self)
-        self.CharX = 0
-        
-        self.timer = QTimer(self)
-        self.timer.setInterval(10)
-        self.timer.timeout.connect(self.timeout)
-        self.timer.start()
-    
-    def CharSetNextFrame(self):
-        self.frame_x += self.xDist
-        if self.count%350<175:
-            self.frame_y = self.yDist*7
-            self.CharX +=5
-        else:
-            self.frame_y = self.yDist*5
-            self.CharX -=5
-        self.count+=1
-        if self.frame_x >= self.xDist*self.col_num:
-            self.frame_x=0
-            
-    def timeout(self):
-        self.CharSetNextFrame()
-        self.update()
+        self.monster = self.Monster(parent = self, row = 4, col = 8, SpriteSheetPath = "./img/monster.png", TimeInterval = 75)
+        self.link = self.MainCharacter(parent = self, row = 8, col = 10, SpriteSheetPath = "./img/link.png", TimeInterval = 10)            
         
     def paintEvent(self, event): # when redrawn
         painter = QtGui.QPainter()
         painter.begin(self)
         pixmap = QPixmap("./img/bg.jpg")
         painter.drawPixmap(self.rect(), pixmap)
-        painter.drawPixmap(self.CharX,360, self.SpriteSheet, self.frame_x, self.frame_y, self.xDist, self.yDist)
-        self.monster._draw(painter)
+        
+        self.link.draw(painter)
+        self.monster.draw(painter)
     
     def keyPressEvent(self, event):
-        if event.key()==Qt.Key_Right:
-            self.frame_y = self.yDist*7
-            self.CharX += 5
-        elif event.key()==Qt.Key_Left:
-            self.frame_y = self.yDist*5
-            self.CharX -= 5
-        elif event.key()==Qt.Key_Up:
-            self.frame_y = self.yDist * 6
-        elif event.key()==Qt.Key_Down:
-            self.frame_y = self.yDist * 4
-        
-        self.CharSetNextFrame()
-        
+        self.link.keyPressEvent(event)
         self.update()
-    
+        
     def keyReleaseEvent(self, event):
-        if not event.isAutoRepeat():
-            print("released")
-            self.frame_x = 0
-            if self.frame_y >= self.yDist*4:
-                self.frame_y -= self.yDist*4
-            self.update()
+        self.link.keyReleaseEvent(event)
+        self.update()
         
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
